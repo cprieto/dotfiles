e3a20302545606bcc9227458a9c0fcd383573633
Cristian Prieto <me@cprieto.com>
2016-09-12 14:21:01 +1000

    Removed not needed plugins for Fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,12 @@
 eval (python -m virtualfish)
 set -x JAVA_HOME (/usr/libexec/java_home)
 
-set -x LESS_TERMCAP_mb (printf "\e[01;31m")  
-set -x LESS_TERMCAP_md (printf "\e[01;31m")  
-set -x LESS_TERMCAP_me (printf "\e[0m")  
-set -x LESS_TERMCAP_se (printf "\e[0m")  
-set -x LESS_TERMCAP_so (printf "\e[01;44;33m")  
-set -x LESS_TERMCAP_ue (printf "\e[0m")  
+set -x LESS_TERMCAP_mb (printf "\e[01;31m")
+set -x LESS_TERMCAP_md (printf "\e[01;31m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
 set -x LESS_TERMCAP_us (printf "\e[01;32m")
 
 # Check if fundle exists, if not, install it
@@ -15,9 +15,6 @@ if not functions -q fundle; eval (curl -sFL https://git.io/fundle-install); end
 # Fundle plugins
 fundle plugin 'edc/bass'
 fundle plugin 'tuvistavie/oh-my-fish-core'
-#fundle plugin 'oh-my-fish/theme-chain'
-#fundle plugin 'oh-my-fish/theme-default'
-#fundle plugin 'oh-my-fish/theme-gnuykeaj'
 fundle plugin 'oh-my-fish/theme-clearance'
 
 fundle init
